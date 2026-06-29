21dc1a94eff775b38c9686c3729cee6a8b81a496
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2026-06-29 13:49:34 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL:"https://noteappbackend-3qit.onrender.com/",
+    baseURL:"http://localhost:8000/",
     headers:{
         "Content-Type":"application/json"
     }
